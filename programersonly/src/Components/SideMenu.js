76aec60e9ca314b085553c92a73df53aeafa4233
Malcolm Machesky <Malcolm9292@gmail.com>
2020-02-15 16:18:17 -0600

    Finished setting up react and started the SideBar

---
+++ programersonly/src/Components/SideMenu.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles } from "@material-ui/core/styles";
+const drawerWidth = 400;
+
+const useStyles = makeStyles(theme => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundImage: `linear-gradient(#cfd9df,#e2ebf0)`,
+    color: "grey"
+  }
+}));
+
+function SideMenu() {
+  const classes = useStyles();
+  return (
+    <Drawer
+      open={true}
+      variant="permanent"
+      anchor="left"
+      className={classes.drawer}
+      classes={{ paper: classes.drawerPaper }}>
+      Test
+    </Drawer>
+  );
+}
+
+export default SideMenu;
