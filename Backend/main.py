0a42fbd3f97036154b06fd4eb9c89c4b1ad1caae
abolejack <abolejack@hawk.iit.edu>
2020-02-16 02:53:44 -0600

    Has http requests

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from .extentions import mongo
 from pymongo import MongoClient
 from passlib.hash import sha256_crypt
@@ -17,24 +17,35 @@ def index():
     users.insert({'ID': 0, 'Name': 'Alex', 'Gender': 'Male', 'Interest': ['Male'],
                             'Bio': "Find some time to do something", 'Code': "Picture", 'Password': "Password", 'Matches':[1,2,3], 'Username':'Axelman03'})
     return '<h1>Added a user!</h1>'
+ID = 0
+
+@main.route('/findUser')
+def findUser():
+    return jsonify({'int':5})
+# def findUser():
+#     reader = users.find_one({'ID': ID})
+#     readerInterest = reader['Interest']
+#     readerGender = reader['Gender']
+#     for genders in readerInterst:
+#         for user in users.find({'Gender': genders}):
+#             userInterest = user['Interest']
+#             for userGender in userInterest:
+#                 if userGender == readerGender:
+#                     return jsonify(user)
 
 
-@main.route('/', methods = ['GET','POST'])
-def findUser(ID):
-    reader = users.find_one({'ID': 0})
-    readerInterest = reader['Interest']
-    readerGender = reader['Gender']
-    for genders in readerInterst:
-        for user in users.find({'Gender': genders}):
-            userInterest = user['Interest']
-            for userGender in userInterest:
-                if userGender == readerGender:
-                    return user
-
-def createUser(name, username, password, link, bio, gender, interest):  #takes in name, username, password, github link, bio, gender, interest
+def createUser(name, username, password, bio, gender, interest):
     count = 1
     for user in users.find():
         count += 1
 
+    ID = count
     password = sha256_crypt.encrypt(password)
-    newUser = {'ID':count, 'Name': name, 'Gender': gender, 'Interest': interest, 'Bio': bio, 'Picture': pic, 'Password':password, 'Matches':[], 'Username':username}
+    newUser = {'ID': ID, 'Name': name, 'Gender': gender, 'Interest': interest, 'Bio': bio, 'Picture': None, 'Password': password, 'Matches': [], 'Username': username}
+    users.insert(newUser)
+
+def sendLinktoUIPath(link):
+    return None
+
+def uploadPicToProfile(pic):  #request UIPath
+    users.update_one({'ID':ID}, {'Picture':pic})
