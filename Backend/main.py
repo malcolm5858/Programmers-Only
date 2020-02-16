0c09a5eae7d2b099ca672f21aed9fbf2a8d9a141
abolejack <abolejack@hawk.iit.edu>
2020-02-16 08:59:52 -0600

    Added some backend functions

--- Backend/main.py
+++ Backend/main.py
@@ -80,12 +80,10 @@ def getUrl():
     return jsonify({'string': img}), 200
 
 
-@main.route('/')
-def index():
-    db = client.Users
-    users = db.UserData
-    users.insert({'ID': 0, 'Name': 'Alex', 'Gender': 'Male', 'Interest': ['Male'],'Bio': "Find some time to do something", 'Code': "Picture", 'Password': "Password", 'Matches': [1, 2, 3], 'Username': 'Axelman03'})
-    return '<h1>Added a user!</h1>'
+# @main.route('/')
+# def index():
+#     users.insert({'ID': 0, 'Name': 'Alex', 'Gender': 'Male', 'Interest': ['Male'],'Bio': "Find some time to do something", 'Code': "Picture", 'Password': "Password", 'Matches': [], 'NotMatches':[], 'Username': 'Axelman03'})
+#     return '<h1>Added a user!</h1>'
 
 
 ID = 0
@@ -103,31 +101,55 @@ def auth():
 
 @main.route('/findUser')
 def findUser():
+    beenSeen = False
     reader = users.find_one({'ID': ID})
     readerInterest = reader['Interest']
     readerGender = reader['Gender']
+    readerMatches = reader['Matches']
+    readerDismatches = reader['NotMatches']
     for genders in readerInterst:
         for user in users.find({'Gender': genders}):
-            userInterest = user['Interest']
-            for userGender in userInterest:
-                if userGender == readerGender:
-                    return jsonify(user)
+            for match in readerMatches:
+                if user['ID'] == match:
+                    beenSeen = True
+            for dismatch in readerDismatches:
+                if user['ID'] == dismatch:
+                    beenSeen = True
+            if beenSeen == False:
+                userInterest = user['Interest']
+                for userGender in userInterest:
+                    if userGender == readerGender:
+                        return jsonify(user)
 
 
-def createUser(name, username, password, bio, gender, interest):
+def createUser(name, username, password, bio, gender, interest, link):
     count = 1
     for user in users.find():
         count += 1
 
     ID = count
     password = sha256_crypt.encrypt(password)
+    img = addUser(link)
     newUser = {'ID': ID, 'Name': name, 'Gender': gender, 'Interest': interest, 'Bio': bio,
-               'Picture': None, 'Password': password, 'Matches': [], 'Username': username}
+               'Picture': img, 'Password': password, 'Matches': [], 'NotMatches':[], 'Username': username}
     users.insert(newUser)
 
+def match(iid):
+    user = users.find_one({'ID': ID})
+    userMatches = user['Matches'] + iid
+    users.update_one(user, userMatches)
+    #return findUser
 
-def sendLinktoUIPath(link):
-    return None
+def notMatch(iid):
+    user = users.find_one({'ID': ID})
+    userNotMatches = user['NotMatches'] + iid
+    users.update_one(user, userNotMatches)
+    #return findUser
+
+def sendName(iid):
+    user = users.find_one({'ID': iid})
+    name = user['Name']
+    return name
 
 
 def uploadPicToProfile(pic):  # request UIPath
