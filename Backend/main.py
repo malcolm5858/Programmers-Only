5993f0472de1c957fa0f9df03b196ed081f7b0cf
abolejack <abolejack@hawk.iit.edu>
2020-02-16 04:41:46 -0600

    pushing

--- Backend/main.py
+++ Backend/main.py
@@ -2,12 +2,82 @@ from flask import Blueprint, jsonify
 from .extentions import mongo
 from pymongo import MongoClient
 from passlib.hash import sha256_crypt
+import requests
 
 main = Blueprint('main', __name__)
 client = MongoClient(
     'mongodb+srv://dbUser:dbUserPassword@programmersonly-vbfpo.gcp.mongodb.net/test?retryWrites=true&w=majority')
 db = client.Users
 users = db.UserData
+
+print("test")
+headers = {'Content-Type': 'application/json', 'X-UIPATH-TenantName': 'IITDefaultym3f337016'}
+payload = {"grant_type": "refresh_token",
+    "client_id": "8DEv1AMNXczW3y4U15LL3jYf62jK93n5",
+    "refresh_token": "IvCJfS2ka_MSscoKuOY-7qik167euQ-VHWKQtW3pb43F1"}
+url = "https://account.uipath.com/oauth/token"
+
+r = requests.post(url, headers=headers, json=payload)
+print("test")
+token = r.json().get("access_token")
+
+
+
+
+#Request 2
+url = "https://platform.uipath.com/iitissuxlz/IITDefaultym3f337016/odata/Releases"
+headers = {'Authorization': 'Bearer ' + token, 
+  'X-UIPATH-TenantName': 'IITDefaultym3f337016'}
+
+r = requests.get(url, headers=headers)
+print("test")
+key=r.json().get("value")[0].get("Key")
+
+
+print("2")
+def addUser():
+    print("into function")
+    testUrl = "https://github.com/malcolm5858/Programmers-Only/blob/master/Backend/main.py"
+    url = "https://platform.uipath.com/iitissuxlz/IITDefaultym3f337016/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs"
+    headers = { 'Content-Type': 'application/json',
+        'X-UIPATH-TenantName': 'IITDefaultym3f337016', 'Authorization': 'Bearer ' + token}
+    payload = {"startInfo": {
+    "ReleaseKey": key,
+    "Strategy": "All",
+    "RobotIds": [],
+    "NoOfRobots": 0,
+    "InputArguments": "{\"CodeURL\":\"" + testUrl + "\"}"
+    }}
+
+    r = requests.post(url, headers=headers, json=payload)
+    postKey = r.json().get('value')[0].get('Key')
+    
+    go = True
+    while (go):
+        headers = {'Authorization': 'Bearer ' + token,
+        'X-UIPATH-TenantName': 'IITDefaultym3f337016'}
+        url = "https://platform.uipath.com/iitissuxlz/IITDefaultym3f337016/odata/Jobs?$orderby=CreationTime desc"
+        r = requests.get(url, headers=headers)
+        i = 0
+        while postKey != r.json().get("value")[i].get("Key"):
+            i+=1
+        if r.json().get("value")[i].get("OutputArguments") == None:
+            print("None")
+        else:
+            print("got the img")
+            image = r.json.get("value")[i].get("OutputArguments")[18:-3]
+            go = False
+    
+    if image:
+        print("we have image")
+
+        
+            
+        
+
+print("HEre")  
+addUser()
+
 
 
 @main.route('/')
@@ -18,6 +88,18 @@ def index():
                             'Bio': "Find some time to do something", 'Code': "Picture", 'Password': "Password", 'Matches':[1,2,3], 'Username':'Axelman03'})
     return '<h1>Added a user!</h1>'
 ID = 0
+
+
+
+@main.route('/')
+def auth():
+
+    return jsonify({
+    "grant_type": "refresh_token",
+    "client_id": "8DEv1AMNXczW3y4U15LL3jYf62jK93n5",
+    "refresh_token": "Xa_238Zt5ZGss2RzhxInG0V2Nqj0Zh6avMj__C9dCn1-Z"
+})
+
 
 @main.route('/findUser')
 def findUser():
